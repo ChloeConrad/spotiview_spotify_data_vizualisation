58481a39a6ab778d4e53841f8dc5fe9e88e7a6fb
Chloé <shloai@localhost.localdomain>
2024-01-10 21:58:17 +0100

    merge elliot + enlever le tooltip dans genre + fonctionnement changement d'utilisateur

--- dataManagement.js
+++ dataManagement.js
@@ -10,7 +10,7 @@ async function loadFullData() {
 
   console.log("Start Loading Full Data")
 
-  if(window.global.subject == "Elliot") {
+  if(window.global.subject == "elliot") {
     window.global.fullHistory = await loadJson("datasets/tracks/light_streaming_history_elliot.json")
     
     console.log(window.global.fullHistory)
@@ -21,10 +21,17 @@ async function loadFullData() {
     window.global.fullArtists = d3.group(await loadJson("datasets/tracks/artists_elliot.json"), d=>d.id)
     window.global.fullAlbums  = d3.group(await loadJson("datasets/tracks/albums_elliot.json"), d=>d.id)
     window.global.fullTracks  = d3.group(await loadJson("datasets/tracks/tracks_elliot.json"), d=>d.id)
-  } else if(window.global.subject == "Elodie") {
+  } else if(window.global.subject == "elodie") {
     window.global.fullHistory = await loadJson("datasets/tracks/light_streaming_history_elodie.json")
-    window.global.fullArtists = await loadJson("datasets/tracks/artists_elodie.json")
-    window.global.fullAlbums  = await loadJson("datasets/tracks/albums_elodie.json")
+    
+    console.log(window.global.fullHistory)
+
+    window.global.fullHistory.forEach( d => {
+      d.ts = new Date(d.ts)
+    }) 
+    window.global.fullArtists = d3.group(await loadJson("datasets/tracks/artists_elodie.json"), d=>d.id)
+    window.global.fullAlbums  = d3.group(await loadJson("datasets/tracks/albums_elodie.json"), d=>d.id)
+    window.global.fullTracks  = d3.group(await loadJson("datasets/tracks/tracks_elodie.json"), d=>d.id)
   } else {
     console.log("Error Loading Full Data, unknown subject")
   }
